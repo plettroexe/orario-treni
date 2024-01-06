e3b8438c61651861067fb25eaf52355a3b5faf97
mirko <augellomirko@itis-molinari.eu>
2024-01-06 12:32:54 +0100

    login fatta

--- orariotreni.js
+++ orariotreni.js
@@ -1,13 +1,36 @@
-function authenticate() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+access.onclick = () => {
+    const url = 'https://ws.progettimolinari.it/credential/login';
+    const token = '56ba61b3-a7cf-4a2d-be49-911579750e38';
+  
+    const loginData = {
+      username: username.value,
+      password: password.value
+    };
+  
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'key': token
+      },
+      body: JSON.stringify(loginData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Risposta dal server:', data.result);
 
-    // Sostituisci con la tua logica di autenticazione
-    if (username === "admin" && password === "password") {
-        document.getElementById("login-container").style.display = "none";
-        document.getElementById("content-container").classList.remove("hidden");
-        document.getElementById("content-container").classList.add("visible");
+      if (data.result === true) {
+        console.log('Accesso come admin riuscito');
+        document.getElementById('login').classList.add('hidden');
+        document.getElementById('login').classList.remove('visible');
+        document.getElementById('content').classList.remove('hidden');
+        document.getElementById('content').classList.add('visible');
     } else {
-        alert("Credenziali non valide. Riprova.");
-    }
-}
+        console.log('Login fallito. Verifica le credenziali.');
+      }
+    })
+    .catch(error => {
+      console.error('Si è verificato un errore:', error);
+    });
+  
+  };
